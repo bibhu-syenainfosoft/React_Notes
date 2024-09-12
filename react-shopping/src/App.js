99e5b3b300507c7ce6192af786472784bb3328a1
Bibhudatta Behera <143987793+bibhu-syenainfosoft@users.noreply.github.com>
2024-09-12 16:12:23 +0530

    wew

---
+++ react-shopping/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Welcome to React</h2>
+     <p>React is a JS library.</p>
+    </div>
+  );
+}
+
+export default App;
