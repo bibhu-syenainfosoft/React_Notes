98fa8c4311a57c32329689116006acae15d22d5c
Bibhudatta <bibhuduttbehera@gmail.com>
2024-09-28 07:46:32 +0530

    changes done

---
+++ react-shopping/src/components/NetflixFooterComponent.js
@@ -0,0 +1,80 @@
+export function NetflixFooter() {
+    return (
+        <div className="content7">
+            <div className="content77">
+                <a href="tel:+91 000-800-040-1843">Questions? Call 000-800-040-1843</a><br /><br />
+
+                <div className="row mb-2">
+                    <div className="col-md-4">
+                        <a href="#">FAQ</a>
+                    </div>
+                    <div className="col-md-3">
+                        <a href="#">Help center</a>
+                    </div>
+                    <div className="col-md-3">
+                        <a href="#">Account</a>
+                    </div>
+                </div>
+
+                <div className="row mb-2">
+                    <div className="col-md-4">
+                        <a href="#">Media Centre</a>
+                    </div>
+                    <div className="col-md-3">
+                        <a href="#">Investor Relations</a>
+                    </div>
+                    <div className="col-md-3">
+                        <a href="#">Jobs</a>
+                    </div>
+                </div>
+
+                <div className="row mb-2">
+                    <div className="col-md-4">
+                        <a href="#">Ways to Watch</a>
+                    </div>
+                    <div className="col-md-3">
+                        <a href="#">Terms of Use</a>
+                    </div>
+                    <div className="col-md-3">
+                        <a href="#">Privacy</a>
+                    </div>
+                </div>
+
+                <div className="row mb-2">
+                    <div className="col-md-4">
+                        <a href="#">Cookie Preferences</a>
+                    </div>
+                    <div className="col-md-3">
+                        <a href="#">Corporate Information</a>
+                    </div>
+                    <div className="col-md-3">
+                        <a href="#">Contact Us</a>
+                    </div>
+                </div>
+
+                <div className="row mb-5">
+                    <div className="col-md-4">
+                        <a href="#">Speed Test</a>
+                    </div>
+                    <div className="col-md-3">
+                        <a href="#">Legal Notices</a>
+                    </div>
+                    <div className="col-md-3">
+                        <a href="#">Only on Netflix</a>
+                    </div>
+                </div>
+
+                <div className="dropdown mb-3">
+                    <button className="px-3 h-50 btn btn-dark dropdown-toggle border border-secondary bg-black text-white" data-bs-toggle="dropdown">
+                        <span className="bi bi-globe"></span> English
+                    </button>
+                    <div className="dropdown-menu collapse">
+                        <div><a href="#" className="dropdown-item active" id="ab">English</a></div>
+                        <div><a href="#" className="dropdown-item" id="cd">हिन्दी</a></div>
+                    </div>
+                </div><br />
+                <a href="#">Netflix India</a>
+            </div>
+        </div>
+    )
+}
--- react-shopping/src/index.js
+++ react-shopping/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'jquery';
+import "./components/Common.css";
+import { NetflixIndex } from './components/NetflixIndexComponent';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NetflixIndex />
   </React.StrictMode>
 );
 
